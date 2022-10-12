ec9b28484224922bd50976f88bec254b9f048ddb
Saurabh Kumar <akgec.saurabh@gmail.com>
2022-10-12 11:36:57 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 40,
+    depth: 200,
+    modifier: 1,
+    slideShadows: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '"></span>';
+    },
+  },
+});
